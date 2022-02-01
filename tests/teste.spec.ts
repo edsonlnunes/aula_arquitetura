b3af12f12f53f1e8eaf5eecbb162430f4fdccc7d
Edson Martins <edsonmartins3110@gmail.com>
2022-01-31 21:41:18 -0300

    finaliza configuracao do jest

--- tests/teste.spec.ts
+++ tests/teste.spec.ts
@@ -1,8 +1,15 @@
-const soma = (num1: number, num2: number) => num1 + num2;
+const soma = (num1: number, num2: number) => {
+  console.log("dentro da função");
+  return num1 + num2;
+};
 
 describe("Jest test", () => {
   test("Deve somar 5 + 5 e dar 10", () => {
     const resultado = soma(5, 5);
-    expect(resultado).toEqual(15);
+    console.log("teste");
+    console.log("teste 1 ");
+    console.log("teste 2 ");
+    console.log("teste 3");
+    expect(resultado).toEqual(10);
   });
 });
